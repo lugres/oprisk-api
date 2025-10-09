54bc8d4856d9c1661afd5107fd76ad45867f590a
Lugres <lugres@gmail.com>
2025-10-09 18:33:24 -0400

    Implement user API

--- app/app/urls.py
+++ app/app/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from drf_spectacular.views import (
     SpectacularAPIView,
@@ -31,4 +31,5 @@ urlpatterns = [
         SpectacularSwaggerView.as_view(url_name="api-schema"),
         name="api-docs",
     ),
+    path("api/user/", include("users.urls")),
 ]
