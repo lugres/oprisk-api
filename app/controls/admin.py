b569edf95af0b6e565134b9426864f31bf1ef122
Lugres <lugres@gmail.com>
2025-12-04 12:10:35 -0500

    Configure admin interface for controls, update admin config for risks and references to sync

--- app/controls/admin.py
+++ app/controls/admin.py
@@ -1,3 +1,88 @@
-from django.contrib import admin  # noqa
+"""
+Django admin and customizations for models of the controls app.
+"""
 
-# Register your models here.
+from django.contrib import admin
+from .models import Control
+
+
+@admin.register(Control)
+class ControlAdmin(admin.ModelAdmin):
+    """Admin interface for the Control Library."""
+
+    # Organize fields into logical groups
+    fieldsets = (
+        (
+            "Identification",
+            {
+                "fields": (
+                    "title",
+                    "description",
+                    "reference_doc",
+                    "is_active",
+                ),
+            },
+        ),
+        (
+            "Design Characteristics",
+            {
+                "fields": (
+                    ("control_type", "control_nature"),
+                    ("control_frequency", "effectiveness"),
+                ),
+                "description": (
+                    "Define the inherent design of the control mechanism."
+                ),
+            },
+        ),
+        (
+            "Context & Ownership",
+            {
+                "fields": ("business_unit", "business_process", "owner"),
+            },
+        ),
+        (
+            "Audit",
+            {
+                "fields": ("created_by", "created_at", "updated_at"),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    # Columns displayed in the list view
+    list_display = (
+        "title",
+        "control_type",
+        "control_frequency",
+        "effectiveness",
+        "business_unit",
+        "owner",
+        "is_active",
+    )
+
+    # Sidebar filters for finding controls quickly
+    list_filter = (
+        "is_active",
+        "control_type",
+        "control_nature",
+        "control_frequency",
+        "effectiveness",
+        "business_unit",
+    )
+
+    # Search bar config
+    search_fields = ("title", "description", "reference_doc")
+
+    # Read-only audit fields
+    readonly_fields = ("created_by", "created_at", "updated_at")
+
+    # Optimize FK lookups
+    # (Prerequisite: Related admins must have search_fields defined)
+    autocomplete_fields = ("business_unit", "business_process", "owner")
+
+    def save_model(self, request, obj, form, change):
+        """Automatically set created_by on creation."""
+        if not obj.pk:
+            obj.created_by = request.user
+        super().save_model(request, obj, form, change)
--- app/references/admin.py
+++ app/references/admin.py
@@ -7,15 +7,29 @@ from django.contrib import admin
 from references import models
 
 admin.site.register(models.Role)
-admin.site.register(models.BusinessUnit)
+# admin.site.register(models.BusinessUnit)
 admin.site.register(models.BaselBusinessLine)
 # admin.site.register(models.BaselEventType)
-admin.site.register(models.BusinessProcess)
+# admin.site.register(models.BusinessProcess)
 admin.site.register(models.Product)
 
 
 @admin.register(models.BaselEventType)
 class BaselEventTypeAdmin(admin.ModelAdmin):
     # This line is REQUIRED to support autocomplete_fields in RiskAdmin
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+@admin.register(models.BusinessUnit)
+class BusinessUnitAdmin(admin.ModelAdmin):
+    # This line is REQUIRED to support autocomplete_fields in ControlAdmin
+    search_fields = ("name",)
+    list_display = ("name",)
+
+
+@admin.register(models.BusinessProcess)
+class BusinessProcessAdmin(admin.ModelAdmin):
+    # This line is REQUIRED to support autocomplete_fields in ControlAdmin
     search_fields = ("name",)
     list_display = ("name",)
